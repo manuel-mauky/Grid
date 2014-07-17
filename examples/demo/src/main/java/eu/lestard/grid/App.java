c8c2639b8fe6a9851ab9127bce5398afb992d457
Manuel Mauky <manuel.mauky@googlemail.com>
2014-07-17 22:36:07 +0200

    Updated Demo with slider controls to change the size of the grid

--- examples/demo/src/main/java/eu/lestard/grid/App.java
+++ examples/demo/src/main/java/eu/lestard/grid/App.java
@@ -1,14 +1,25 @@
 package eu.lestard.grid;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application{
 
+
+    /**
+     * The states that a grid cell can have in our example
+     */
     public static enum States {
         A,
         B
@@ -20,44 +31,46 @@ public class App extends Application{
     }
 
 
+    private IntegerProperty numberOfRows = new SimpleIntegerProperty(9);
+    private IntegerProperty numberOfColumns = new SimpleIntegerProperty(8);
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Grid");
-
-        AnchorPane root = new AnchorPane();
+        BorderPane root = new BorderPane();
 
 
         GridModel<States> gridModel = new GridModel<>();
+
+        // define the state that will be used as default.
         gridModel.setDefaultState(States.A);
+
+
+        // create the grid view and set the grid model
         GridView<States> gridView = new GridView<>();
         gridView.setGridModel(gridModel);
 
+        // define which color is shown when a cell has a specific state
         gridView.addColorMapping(States.A, Color.YELLOWGREEN);
         gridView.addColorMapping(States.B, Color.ORANGE);
 
 
+        // additionally we add labels that are placed inside the cells
         gridView.addNodeMapping(States.A, (cell) -> new Label("A"));
+        gridView.addNodeMapping(States.B, (cell) -> new Label("B"));
 
 
+        // styling of the strokes between the cells
         gridView.strokeProperty().set(Color.BLACK);
         gridView.strokeWidthProperty().set(1);
 
 
-        AnchorPane.setLeftAnchor(gridView, 0.0);
-        AnchorPane.setRightAnchor(gridView, 0.0);
-        AnchorPane.setBottomAnchor(gridView, 0.0);
-        AnchorPane.setTopAnchor(gridView, 0.0);
 
-        root.getChildren().add(gridView);
+        root.setCenter(gridView);
 
-
-        gridModel.setNumberOfRows(9);
-        gridModel.setNumberOfColumns(5);
-
-        gridModel.getCell(2,4).changeState(States.B);
-
-
-        gridModel.getCells().forEach(cell->{
+        // every time a new cell is added, we add an click listener to it.
+        gridModel.setOnCellAddedHandler((cell)->{
+            // the click handler switches the state of the cells
             cell.setOnClick((event) -> {
                 final States stateBefore = cell.getState();
                 if (stateBefore == States.A) {
@@ -68,8 +81,59 @@ public class App extends Application{
             });
         });
 
-        stage.setScene(new Scene(root, 800,600));
 
+        // bind the number of rows/colums in the grid model
+        gridModel.numberOfColumns().bind(numberOfColumns);
+        gridModel.numberOfRows().bind(numberOfRows);
+
+        root.setBottom(createControls());
+
+        stage.setScene(new Scene(root, 800,600));
         stage.show();
     }
+
+
+    /**
+     * Create a controls panel so that we can control the grid properties.
+     */
+    private VBox createControls(){
+        VBox controlsBox = new VBox();
+        controlsBox.setSpacing(10);
+        controlsBox.setPadding(new Insets(10));
+
+
+        HBox numberOfRowsBox = new HBox();
+        numberOfRowsBox.setSpacing(5);
+        Label numberOfRowsLabel = new Label();
+        Slider numberOfRowsSlider = new Slider();
+        numberOfRowsBox.getChildren().addAll(numberOfRowsLabel, numberOfRowsSlider);
+
+
+        numberOfRowsLabel.textProperty().bind(Bindings.concat("Rows: ", numberOfRows));
+        numberOfRowsSlider.valueProperty().bindBidirectional(numberOfRows);
+
+        numberOfRowsSlider.setMin(3);
+        numberOfRowsSlider.setMax(30);
+
+
+
+
+        HBox numberOfColumnsBox = new HBox();
+        numberOfColumnsBox.setSpacing(5);
+        Label numberOfColumnsLabel = new Label();
+        Slider numberOfColumnsSlider = new Slider();
+        numberOfColumnsBox.getChildren().addAll(numberOfColumnsLabel, numberOfColumnsSlider);
+
+        numberOfColumnsLabel.textProperty().bind(Bindings.concat("Columns:", numberOfColumns));
+        numberOfColumnsSlider.valueProperty().bindBidirectional(numberOfColumns);
+
+        numberOfColumnsSlider.setMin(3);
+        numberOfColumnsSlider.setMax(30);
+
+
+        controlsBox.getChildren().addAll(numberOfRowsBox, numberOfColumnsBox);
+
+        return controlsBox;
+    }
 }
+
